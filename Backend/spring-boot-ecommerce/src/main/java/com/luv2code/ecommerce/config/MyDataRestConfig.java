f63b6d47a059c0ad77cc07be82219264da6b6c96
fathyOsama <fathy.osama.eg@gmail.com>
2026-07-21 11:51:23 +0300

    Add jakarta.persistence.EntityManager and jakarta.persistence.metamodel.EntityType

--- Backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ Backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -7,6 +7,10 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
+
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
