fc53765bd64f072f66a7bf99fb3022f5dd44601c
fathyOsama <fathy.osama.eg@gmail.com>
2026-07-22 16:23:19 +0300

    expose entity IDs in Spring Data REST responses

--- Backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ Backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -11,6 +11,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.metamodel.EntityType;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 
 @Configuration
@@ -40,5 +43,28 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 
+        // call an internal helper method
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        // expose entity ids
+        //
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // - create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // - get the entity types for the entities
+        for (EntityType tempEntityType : entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // - expose the entity ids for the array of entity/domain type
+        Class[] domainType = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainType);
     }
 }
