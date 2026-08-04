dab543abada289fd469157ee7e3fe0a7b053359a
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-04 22:56:21 +0300

    Kwyword Search Add pagination support  to productService

--- Frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -55,6 +55,17 @@ export class ProductService {
 
   }
 
+  searchProductsPaginate(thePage: number,
+                         thePageSize: number,
+                         theKeyword: string): Observable<GetResponseProducts> {
+
+    // @ need to build URL based on Keyword, page and size
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
 
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
