53a26ceb38e36489321de90592f4cd2179ccda25
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-09 09:35:18 +0300

    Adding click handler for "Add to Cart" buttom

--- Frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ Frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Product } from 'src/app/common/product';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -10,10 +12,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  product! : Product;
+  product?: Product;
 
   constructor (
     private productService : ProductService,
+    private cartService: CartService,
     private route : ActivatedRoute
   ) {}
 
@@ -32,5 +35,14 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     )
+  }
+
+  addToCart() {
+    if (!this.product) {
+      return;
+    }
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
