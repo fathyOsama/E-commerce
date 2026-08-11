ba145f53be843fdac634c26fadbd8e33f821d6ef
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-11 18:04:47 +0300

    Repeat process for Remove button

--- Frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ Frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -48,4 +48,9 @@ export class CartDetailsComponent implements OnInit{
   decrementQuantity (theCartItem: CartItem) {
     this.cartService.decrementQuantity(theCartItem);
   }
+
+  remove(theCartItem: CartItem) {
+   this.cartService.remove(theCartItem);
+  }
+
 }
