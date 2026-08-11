3f6bedd22bb9edc2e4654241416c680411592d19
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-11 17:51:14 +0300

    feat: Update CartDetailsComponent with click handler method

--- Frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
+++ Frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -41,7 +41,11 @@ export class CartDetailsComponent implements OnInit{
     this.cartService.computeCartTotals();
   }
 
-  incrementQuantity(theCartItem: CartItem) {
+  incrementQuantity (theCartItem: CartItem) {
     this.cartService.addToCart(theCartItem);
+  }
+
+  decrementQuantity (theCartItem: CartItem) {
+    this.cartService.decrementQuantity(theCartItem);
   }
 }
