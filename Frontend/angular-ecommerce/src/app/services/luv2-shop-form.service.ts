855e1d742aeca2b15b4dec8cc9e76e32cf73e518
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-15 13:53:16 +0300

    Add methods to the form service for months and years

--- Frontend/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
+++ Frontend/angular-ecommerce/src/app/services/luv2-shop-form.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,36 @@ import { Injectable } from '@angular/core';
 export class Luv2ShopFormService {
 
   constructor() { }
+
+  getCreaditCredMonths (startMonth: number) : Observable<number[]> {
+
+    let data: number[] = [];
+
+    // build an array for "Month" dropdown list
+    // - start at current month and loop until
+
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+      data.push(theMonth);
+    }
+
+    return of(data);
+  }
+
+  getCreaditCredYears (): Observable<number[]> {
+
+    let data: number[] = [];
+
+    // build an array for "Year" downlist list
+    // - start at current year and loop for next 10 years
+
+    const startYear: number = new Date().getFullYear();
+    const endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
+      data.push(theYear);
+    }
+
+    return of (data);
+  }
+
 }
