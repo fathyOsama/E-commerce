49fb49c8a7fe85de9cf18c9d9f015576551836e6
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-17 12:15:42 +0300

    feat: log shipping address country and state on checkout

--- Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -100,10 +100,13 @@ export class CheckoutComponent implements OnInit {
     }
   }
 
-  onSubmit() {
+ onSubmit() {
     console.log("Handling the submit button");
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
+
+    console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
+    console.log("The shipping address state is " + this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
   }
 
   handleMonthsAndYears() {
@@ -139,8 +142,8 @@ export class CheckoutComponent implements OnInit {
     const countryCode = formGroup?.value.country.code;
     const countryName = formGroup?.value.country.name;
 
-    console.log(`{formGroupName} country code: ${countryCode}`);
-    console.log(`{formGroupName} country name: ${countryName}`);
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
 
     this.luv2ShopFormService.getStates(countryCode).subscribe(
       data => {
