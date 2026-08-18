5da2d9ef5aa87f4196bd669d5151da2761592946
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-18 15:00:17 +0300

    feat: add validation for shipping address fields

--- Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -43,14 +43,23 @@ export class CheckoutComponent implements OnInit {
         email: new FormControl('',
                               [Validators.required,
                               Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
-        ),
+        )
       }),
       shippingAddress: this.formBuilder.group({
-        street: [''],
-        city: [''],
-        state: [''],
-        country: [''],
-        zipCode: [''],
+        street: new FormControl('',
+                              [Validators.required, Validators.minLength(2),
+                              Luv2ShopValidators.notOnlyWhitespace]
+        ),
+        city: new FormControl('',
+                            [Validators.required, Validators.minLength(2),
+                            Luv2ShopValidators.notOnlyWhitespace]
+        ),
+        state: new FormControl('', [Validators.required]),
+        country: new FormControl('', [Validators.required]),
+        zipCode: new FormControl('',
+                               [Validators.required, Validators.minLength(2),
+                               Luv2ShopValidators.notOnlyWhitespace]
+        )
       }),
       billingAddress: this.formBuilder.group({
         country: [''],
@@ -110,6 +119,26 @@ export class CheckoutComponent implements OnInit {
 
   get email() {
     return this.checkoutFormGroup.get('customer.email');
+  }
+
+  get shippingAddressStreet() {
+    return this.checkoutFormGroup.get('shippingAddress.street');
+  }
+
+  get shippingAddressCity() {
+    return this.checkoutFormGroup.get('shippingAddress.city');
+  }
+
+  get shippingAddressState() {
+    return this.checkoutFormGroup.get('shippingAddress.state');
+  }
+
+  get shippingAddressCountry() {
+    return this.checkoutFormGroup.get('shippingAddress.country');
+  }
+
+  get shippingAddressZipCode() {
+    return this.checkoutFormGroup.get('shippingAddress.zipCode');
   }
 
   copyShippingAddressToBillingAddress(event: Event) {
