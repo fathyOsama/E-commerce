7132be940880af5ac9c79d807d285f67301e7b9b
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-18 14:23:15 +0300

    feat: add whitespace validation for customer names

--- Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Country } from 'src/app/common/country';
 import { State } from 'src/app/common/state';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
+import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
 
 @Component({
   selector: 'app-checkout',
@@ -26,14 +27,23 @@ export class CheckoutComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private luv2ShopFormService: Luv2ShopFormService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
+        firstName: new FormControl('',
+                                  [Validators.required, Validators.minLength(2),
+                                  Luv2ShopValidators.notOnlyWhitespace]
+        ),
+        lastName: new FormControl('',
+                                [Validators.required, Validators.minLength(2),
+                                Luv2ShopValidators.notOnlyWhitespace]
+        ),
+        email: new FormControl('',
+                              [Validators.required,
+                              Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
+        ),
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -118,7 +128,7 @@ export class CheckoutComponent implements OnInit {
     }
   }
 
- onSubmit() {
+  onSubmit() {
     console.log("Handling the submit button");
 
     if (this.checkoutFormGroup.invalid) {
