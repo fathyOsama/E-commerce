159cb6e3dccf1a08c3ba9f18112fc32139808728
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-20 09:18:16 +0300

    feat: subscribe to cart totals in checkout

--- Frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ Frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -1,4 +1,4 @@
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { CartItem } from './../common/cart-item';
 import { Injectable } from '@angular/core';
 
@@ -9,8 +9,8 @@ export class CartService {
 
   cartItems: CartItem[] = [];
 
-  totalPrice: Subject<number> = new Subject<number>();
-  totalQuantity: Subject<number> = new Subject<number>();
+  totalPrice: Subject<number> = new BehaviorSubject<number>(0);
+  totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
   constructor() { }
 
