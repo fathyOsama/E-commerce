7abbd9fb87b09defc586b60d95376f44c42a89e0
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-20 13:07:33 +0300

    feat: Add CheckoutService and CheckoutServiceImpl

---
+++ Backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/serviceImpl/CheckoutServiceImpl.java
@@ -0,0 +1,22 @@
+package com.luv2code.ecommerce.serviceImpl;
+
+import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.Purchase;
+import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.service.CheckoutService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService {
+
+    private CustomerRepository customerRepository;
+
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public PurchaseResponse placeOrder(Purchase purchase) {
+        return null;
+    }
+}
