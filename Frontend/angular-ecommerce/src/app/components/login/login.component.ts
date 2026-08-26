3bbe2ec74c0504fba4202faa244cfb67b93d577e
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-26 15:39:57 +0300

    feat: Add Auth0 login page

---
+++ Frontend/angular-ecommerce/src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private auth: AuthService) {}
+
+  login(): void {
+    this.auth.loginWithRedirect();
+  }
+}
