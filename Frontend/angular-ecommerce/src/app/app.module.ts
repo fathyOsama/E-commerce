c9d2b4e0b6dd3bca05bcdc84bfca9efad676c75c
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-27 12:01:58 +0300

    feat: Add Auth0 protected members route and interceptor

--- Frontend/angular-ecommerce/src/app/app.module.ts
+++ Frontend/angular-ecommerce/src/app/app.module.ts
@@ -3,69 +3,69 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductService } from './services/product.service';
-import { Routes, RouterModule } from '@angular/router';
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductService } from './services/product.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginStatusComponent } from './components/login-status/login-status.component';
-
-import { LoginComponent } from './components/login/login.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AuthModule } from '@auth0/auth0-angular';
-
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { AuthGuard, AuthHttpInterceptor, AuthModule } from '@auth0/auth0-angular';
 import myAppConfig from './config/my-app-config';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
+import { AppRoutingModule } from './app-routing.module';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  {path: 'members', component: MembersPageComponent,  canActivate: [AuthGuard] },
 
-  { path: 'checkout', component: CheckoutComponent},
-  { path: 'cart-details', component: CartDetailsComponent},
-  { path: 'products/:id', component: ProductDetailsComponent},
-  { path: 'search/:keyword', component: ProductListComponent},
-  { path: 'category/:id', component: ProductListComponent },
-  { path: 'category', component: ProductListComponent },
-  { path: 'products', component: ProductListComponent },
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: '**', redirectTo: 'products', pathMatch: 'full' },
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'cart-details', component: CartDetailsComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
 ];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCategoryMenuComponent,
-    SearchComponent,
     ProductDetailsComponent,
+    ProductCategoryMenuComponent,
+    MembersPageComponent,
+    CheckoutComponent,
     CartStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent,
-    LoginComponent,
+    SearchComponent,
     LoginStatusComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
+    AppRoutingModule,
     NgbModule,
     ReactiveFormsModule,
     AuthModule.forRoot({
       ...myAppConfig.auth,
+      // cacheLocation: 'localstorage',
       httpInterceptor: {
         ...myAppConfig.httpInterceptor,
       },
-    }),
+    })
   ],
-  providers: [
-    ProductService
-  ],
+  providers: [ProductService,  {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true, },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
