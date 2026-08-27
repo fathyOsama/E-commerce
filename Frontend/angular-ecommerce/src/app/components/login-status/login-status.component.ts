2e311f953cb840eae51b7a10ec6a68cf62182da6
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-27 12:03:43 +0300

    feat: Enhance login status with user session and members access

--- Frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ Frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -1,57 +1,50 @@
-import { Component, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-login-status',
+  // template: '<button (click)="login()">Log in</button>',
   templateUrl: './login-status.component.html',
   styleUrls: ['./login-status.component.css']
 })
-export class LoginStatusComponent implements OnInit {
+export class LoginStatusComponent {
 
-  isAuthenticated = false;
-  userFullName = '';
+  isAuthenticated: boolean = false;
+  profileJson: string | undefined;
+  userEmail: string | undefined;
+  storage: Storage = sessionStorage;
 
-  constructor(
-    private auth: AuthService,
-    @Inject(DOCUMENT) private doc: Document
-  ) { }
+  constructor(private auth: AuthService, @Inject(DOCUMENT) private doc: Document) {}
 
   ngOnInit(): void {
-
     this.auth.isAuthenticated$.subscribe(
-      authenticated => {
-
+      (authenticated: boolean) => {
         this.isAuthenticated = authenticated;
-
-        if (authenticated) {
-          this.getUserDetails();
-        }
-
+        console.log('User is authenticated: ', this.isAuthenticated);
+      }
+    );
+    this.auth.user$.subscribe(
+      (user) => {
+        this.userEmail = user?.email;
+         // now store the email in browser storage
+         this.storage.setItem('userEmail', JSON.stringify(this.userEmail));
+        console.log('User ID: ', this.userEmail);
       }
     );
   }
 
-  getUserDetails(): void {
-
-    this.auth.user$.subscribe(user => {
-
-      this.userFullName =
-        user?.name ||
-        user?.email ||
-        '';
-
-    });
+  login() {
+    this.auth.loginWithRedirect();
   }
 
   logout(): void {
-
+    // this.auth.logout({ returnTo: this.doc.location.origin });
     this.auth.logout({
       logoutParams: {
         returnTo: this.doc.location.origin
       }
     });
-
   }
 
 }
