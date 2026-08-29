031d91f8198e4769f4734790edb31598045a691a
fathyOsama <fathy.osama.eg@gmail.com>
2026-08-29 14:17:27 +0300

    feat: Persist cart data using session storage

--- Frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ Frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -13,6 +13,7 @@ export class CartService {
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
   storage: Storage = sessionStorage;
+  //storage: Storage = localStorage;
 
   constructor() {
 
@@ -70,6 +71,9 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    // persist cart data
+    this.persistCartItems();
   }
 
   persistCartItems() {
